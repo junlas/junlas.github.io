53c4c72ab1ea1e6ecab5febf7e9fb62b3cc6cf54
junlas lyu <junlas@hotmail.com>
2024-03-18 16:02:38 +0800

    fit phone screen

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,1 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/LoadScene.fire",orientation:"landscape",jsList:[]};
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/LoadScene.fire",orientation:"",jsList:[]};
